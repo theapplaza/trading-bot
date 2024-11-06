ef5760c92f85aa31c40f3f07996def4a53132ab1
Oyesola Ogundele <oyekunmi@gmail.com>
2024-11-06 20:30:28 +0400

    feat: Initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Applaza trading bot with different API for creating and opening orders and fetching data. It integrates with different data providers and platform")
+}
