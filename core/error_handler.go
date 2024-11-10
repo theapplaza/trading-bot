c334e014702816074bdef6e013829d4f734bc53a
Oyesola Ogundele <oyekunmi@gmail.com>
2024-11-10 10:46:46 +0400

    feat: remove error channel in favour of context

--- core/error_handler.go
+++ core/error_handler.go
@@ -8,26 +8,19 @@ import (
 	"syscall"
 )
 
-// Global channels
-var (
-	errorChannel = make(chan error, 10)
-)
-
 // HandleErrorsAndShutdown listens for errors or signals, then initiates shutdown.
 func HandleErrorsAndShutdown(cancel context.CancelFunc) {
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	sig := <-sigChan
+
+	sig := <-sigChan // block and wait for an OS signal
 	log.Printf("Received signal: %v. Initiating shutdown...", sig)
 
 	cancel() //signals all goroutines to stop
 
-	// Wait for all streamers to finish
+	// Wait for all streamers to finish, so that we don't have hanging goroutines/resources running in the background
+	//even though our process has exited
 	streamersGroup.Wait()
 	log.Println("Program has exited gracefully.")
-}
-
-func GetErrorChannel() chan error {
-	return errorChannel
 }
